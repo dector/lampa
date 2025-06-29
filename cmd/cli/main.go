7e67aabaa183e27fc5cfe81bf6360715ed9c4387
D <dector@dector.space>
2025-06-29 03:10:49 +0200

    feat: add CLI commands

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,42 +2,70 @@ package main
 
 import (
 	"fmt"
-	"lampa/internal"
 	"os"
+
+	. "lampa/internal/globals"
+
+	"github.com/samber/lo"
 )
 
 func main() {
-	file := ""
-	configName := ""
-	if len(os.Args) > 2 {
-		file = os.Args[1]
-		configName = os.Args[2]
-	} else {
-		os.Stderr.WriteString("usage: lampa <file> <configuration name>\n")
-		os.Exit(1)
-	}
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		os.Stderr.WriteString("file does not exist\n")
-		os.Exit(1)
+	command := lo.NthOrEmpty(os.Args, 1)
+
+	switch command {
+	case "collect":
+		execCollect()
+	case "help":
+		printUsage()
+		os.Exit(0)
+	case "version":
+		fmt.Println(G.Version)
+		os.Exit(0)
+	default:
+		printUsage()
+		os.Exit(2)
 	}
 
-	data, err := os.ReadFile(file)
-	if err != nil {
-		panic(err)
-	}
-	content := string(data)
+	// file := ""
+	// configName := ""
+	// if len(os.Args) > 2 {
+	// 	file = os.Args[1]
+	// 	configName = os.Args[2]
+	// } else {
+	// 	os.Stderr.WriteString("usage: lampa <file> <configuration name>\n")
+	// 	os.Exit(1)
+	// }
+	// if _, err := os.Stat(file); os.IsNotExist(err) {
+	// 	os.Stderr.WriteString("file does not exist\n")
+	// 	os.Exit(1)
+	// }
 
-	tree, err := internal.ParseTreeFromOutput(content, configName)
-	if err != nil {
-		panic(err)
-	}
+	// data, err := os.ReadFile(file)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// content := string(data)
 
-	// jsonTree, err := json.MarshalIndent(tree.Summary, "", "  ")
+	// tree, err := internal.ParseTreeFromOutput(content, configName)
 	// if err != nil {
 	// 	panic(err)
 	// }
 
-	for _, it := range tree.Summary {
-		fmt.Println(it.String())
-	}
+	// // jsonTree, err := json.MarshalIndent(tree.Summary, "", "  ")
+	// // if err != nil {
+	// // 	panic(err)
+	// // }
+
+	// for _, it := range tree.Summary {
+	// 	fmt.Println(it.String())
+	// }
+}
+
+func execCollect() {
+	fmt.Println("Not Implemented Yet")
+	os.Exit(127)
+}
+
+func printUsage() {
+	fmt.Println("usage: lampa ...")
 }
---
+++ internal/globals/globals.go
@@ -0,0 +1,9 @@
+package globals
+
+type Globals struct {
+	Version string
+}
+
+var G = Globals{
+	Version: "0.0.1",
+}
