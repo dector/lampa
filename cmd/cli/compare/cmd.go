b2ea7629c737a80348ac6fb8a1d7b70a1775f104
D <dector@dector.space>
2025-07-01 19:44:05 +0200

    ref: extract report parsing to a function

--- cmd/cli/compare/cmd.go
+++ cmd/cli/compare/cmd.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"lampa/internal/report"
-	"log"
 	"os"
 	"strings"
 
@@ -17,55 +16,25 @@ func ActionCmdCompare(context context.Context, cmd *cli.Command) error {
 		return fmt.Errorf("expected exactly two files to compare, got %d", cmd.NArg())
 	}
 
-	file1 := cmd.Args().Get(0)
-	file2 := cmd.Args().Get(1)
-
-	info1, err := os.Stat(file1)
+	file1, err := checkReportFile(cmd.Args().Get(0))
 	if err != nil {
-		return fmt.Errorf("could not stat %s: %v", file1, err)
+		return err
 	}
-	if info1.IsDir() {
-		return fmt.Errorf("%s exists but is a directory, not a file", file1)
-	}
-
-	info2, err := os.Stat(file2)
+	file2, err := checkReportFile(cmd.Args().Get(1))
 	if err != nil {
-		return fmt.Errorf("could not stat %s: %v", file2, err)
-	}
-	if info2.IsDir() {
-		return fmt.Errorf("%s exists but is a directory, not a file", file2)
+		return err
 	}
 
-	data1, err := os.ReadFile(file1)
+	dep1, release1, err := readReport(file1)
 	if err != nil {
-		return fmt.Errorf("could not read %s: %v", file1, err)
+		return err
 	}
-
-	var report1 report.Report
-	if err := json.Unmarshal(data1, &report1); err != nil {
-		return fmt.Errorf("could not parse %s as report.Report: %v", file1, err)
-	}
-
-	data2, err := os.ReadFile(file2)
+	dep2, release2, err := readReport(file2)
 	if err != nil {
-		return fmt.Errorf("could not read %s: %v", file2, err)
+		return err
 	}
 
-	var report2 report.Report
-	if err := json.Unmarshal(data2, &report2); err != nil {
-		log.Fatalf("error: could not parse %s as report.Report: %v", file2, err)
-	}
-
-	fmt.Printf("Comparing releases %s...%s\n", report1.Context.Git.Commit, report2.Context.Git.Commit)
-
-	dep1, err := parseDependencies(report1)
-	if err != nil {
-		return fmt.Errorf("could not parse %s as report.Report: %v", file1, err)
-	}
-	dep2, err := parseDependencies(report2)
-	if err != nil {
-		return fmt.Errorf("could not parse %s as report.Report: %v", file2, err)
-	}
+	fmt.Printf("Comparing releases %s...%s\n", release1, release2)
 
 	newDeps := findNewDeps(dep1, dep2)
 	removedDeps := findRemovedDeps(dep1, dep2)
@@ -180,4 +149,35 @@ func findChangedDeps(oldDeps, newDeps []Dependency) []DependencyChange {
 		}
 	}
 	return result
+}
+
+func checkReportFile(path string) (string, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return "", fmt.Errorf("could not stat %s: %v", path, err)
+	}
+	if info.IsDir() {
+		return "", fmt.Errorf("%s exists but is a directory, not a file", path)
+	}
+
+	return path, nil
+}
+
+func readReport(file string) ([]Dependency, string, error) {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, "", fmt.Errorf("could not read %s: %v", file, err)
+	}
+
+	var report report.Report
+	if err := json.Unmarshal(data, &report); err != nil {
+		return nil, "", fmt.Errorf("could not parse %s as report: %v", file, err)
+	}
+
+	dep, err := parseDependencies(report)
+	if err != nil {
+		return nil, "", fmt.Errorf("could not parse %s as report.Report: %v", file, err)
+	}
+
+	return dep, report.Context.Git.Commit, err
 }
